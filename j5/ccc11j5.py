e3493a892219a8c8cc303ddb09aa69aef06c8e71
Scr4tch587 <68821592+Scr4tch587@users.noreply.github.com>
2023-01-31 10:51:28 -0800

    Create ccc11j5.py

---
+++ j5/ccc11j5.py
@@ -0,0 +1,16 @@
+#AC
+n = int(input())
+
+friends = [int(input()) for i in range(n-1)]
+
+def ways(friend):
+    count = 1
+
+    for i in range(0, n-1):
+        if (friends[i] == friend):
+            count *= 1 + ways(i + 1)
+
+    
+    return count
+
+print(ways(n))
